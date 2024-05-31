6cb13f91d7482310f4a7a380c0a9a7c0b30e83a9
minyoung <kevinbaek01@gmail.com>
2024-06-01 01:14:34 +0900

    add porfolio framework

--- trading bot/pyrobot/portfolio.py
+++ trading bot/pyrobot/portfolio.py
@@ -4,6 +4,7 @@ from typing import Union
 from typing import Optional
 
 class Portfolio():
+
     def __init__(self, account_number: Optional[str]):
         self.positions = {}
         self.positions_count = 0
@@ -21,3 +22,55 @@ class Portfolio():
         self.positions[symbol]['asset_type'] = asset_type
 
         return self.positions
+    
+    def add_positions(self, positions: List[dict]) -> dict:
+
+        if isinstance(positions, list):
+            for position in positions:
+
+                self.add_position(
+                    symbol=position['symbol'],
+                    asset_type=position['asset_type'],
+                    purchase_date=position.get('purchase_date', None),
+                    purhcase_price=position.get('purhcase_price', 0.0),
+                    quantity=position.get('quantity', 0)
+                )
+
+                return self.positions
+        
+        else:
+            raise TypeError("Positions must be a list of dictionaries.")
+        
+    def remove_position(self, symbol: str) -> Tuple[bool, str]:
+
+        if symbol in self.positions:
+            del self.positions[symbol]
+            return (True, "{Symbol} was successfully removed.".format(symbol=symbol))
+        else:
+            (False, "{Symbol} did not exist in the porfolio.".format(symbol=symbol))
+
+    def in_porfolio(self, symbol: str) -> bool:
+
+        if symbol in self.positions:
+            return True
+        else:
+            return False
+        
+    def is_profitable(self, symbol: str, current_price: float) -> bool:
+
+        # Grab the purchase price
+        purchase_price = self.positions[symbol]['purchase_price']
+
+        if (purchase_price <= current_price):
+            return True
+        elif (purchase_price > current_price):
+            return False
+        
+    def total_allocation(self):
+        pass
+
+    def risk_exposure(self):
+        pass
+
+    def total_market_value(self):
+        pass
