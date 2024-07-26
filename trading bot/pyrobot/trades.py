76fa9e23e9f206ec5e4907482538dfe4a5882cc2
minyoung <kevinbaek01@gmail.com>
2024-07-26 10:54:11 +0900

    update trades.py

--- trading bot/pyrobot/trades.py
+++ trading bot/pyrobot/trades.py
@@ -75,3 +75,69 @@ class Trade():
             self.order['stopPriceLinkType'] = ""
             self.order['stopPriceOffset'] = 0.00
             self.order['stopType'] = 'STANDARD'
+
+        self.enter_or_exit = enter_or_exit
+        self.side = side
+        self.order_type = order_type
+        self.price = price
+
+        # Store important info for later use.
+        if order_type == 'stop':
+            self.stop_price = price
+        elif order_type == 'stop-lmt':
+            self.stop_price = price
+            self.stop_limit_price = stop_limit_price
+        else:
+            self.stop_price = 0.0
+
+        # Store important side info.
+        if self.enter_or_exit == 'enter':
+            self.enter_or_exit_opposite = 'exit'
+        elif self.enter_or_exit == 'exit':
+            self.enter_or_exit_opposite = 'enter'
+
+        if self.side == 'long':
+            self.side_opposite = 'short'
+        elif self.side == 'short':
+            self.side_opposite = 'long'
+
+        return self.order
+        
+    def instrument(self, symbol: str, quantity: int, asset_type: str, sub_asset_type: str = None, order_leg_id: int = 0) -> dict:
+
+        leg = self.order['orderLegCollection'][order_leg_id]
+
+        leg['instrument']['symbol'] = symbol
+        leg['instrument']['assetType'] = asset_type
+        leg['quantity'] = quantity
+
+        self.order_size = quantity
+        self.symbol = symbol
+        self.asset_type = asset_type
+
+        return leg
+
+    def good_till_cancel(self, cancel_time: datetime) -> None:
+
+        self.order['duration'] = 'GOOD_TILL_CANCEL'
+        self.order['cancelTime'] = cancel_time.isoformat()
+
+    def modify_side(self, side: Optional[str], order_leg_id: int = 0) -> None:
+
+        if side and side not in ['buy', 'sell', 'sell_short', 'buy_to_cover']:
+            raise ValueError("You passed through an invalid side.")
+        
+        if side:
+            self.side['orderLegCollection'][order_leg_id]['instructions'] = side.upper()
+        else:
+            self.order['orderLegCollection'][order_leg_id]['instructions'] = self.order_instructions[self.enter_or_exit][self.side_opposite]
+
+    def add_box_range(self, profit_size: float = 0.00, percentage: bool = False, stop_limit: bool = False):
+
+        if not self._triggered_added:
+            self._convert_to_trigger()
+
+            self.add_take_profit(profit_size=profit_size, percentage=percentage)
+
+            if not stop_limit:
+                self.add_stop_loss(stop_size=profit_size, percentage=percentage)
